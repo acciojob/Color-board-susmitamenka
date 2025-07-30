ea4a5edc95d2b14058465148a51d2aa1077417fc
susmitamenka <susmitaprasad73@gmail.com>
2025-07-30 18:01:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+let container = document.querySelector(".container")
+let numberOfSquare = 800
+
+for(let i=0; i<numberOfSquare; i++){
+	let squarebox = document.createElement("div")
+	 squarebox.classList.add('square');
+
+	squarebox.addEventListener("mouseover",()=>{
+		let color = getRandomColor()
+		squarebox.style.background = color
+         setTimeout(()=>{
+		squarebox.style.background = "#222"
+		},1000)
+	})
+	container.append(squarebox)
+}
+function getRandomColor() {
+	const colors = ['#ff595e', '#1982c4', '#6a4c93', '#8ac926', '#ffca3a'];
+  return colors[Math.floor(Math.random() * colors.length)];
+}
